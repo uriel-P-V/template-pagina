27db1a5aa49d13b3290ac51535e14a7424a1f80d
Perez2121pv <uriperez4142@gmail.com>
2024-07-03 19:16:44 -0600

    Avance Miercoles 3/7/24

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 document.querySelectorAll('.faq-question').forEach(item => {
     item.addEventListener('click', event => {
         const answer = item.nextElementSibling;
-        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+        const isActive = item.classList.contains('active');
+        if (!isActive) {
+            answer.style.display = 'block';
+            item.classList.add('active');
+        } else {
+            answer.style.display = 'none';
+            item.classList.remove('active');
+        }
     });
 });
